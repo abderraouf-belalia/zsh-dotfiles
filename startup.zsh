24194cd8e9e1fd30ed479f6aed3087cb1a36b43d
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2024-03-11 15:49:58 +0100

    Removed `exec` from tmux launch. Added attach on startup.

--- startup.zsh
+++ startup.zsh
@@ -1,6 +1,6 @@
 # Tmux
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  exec tmux
+  tmux attach || tmux
 fi
 
 # NVM
