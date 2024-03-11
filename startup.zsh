ca4f883233151db6fc8180cefe717f06aea0e217
abderraouf-belalia <abderraoufbelalia@gmail.com>
2024-03-11 16:44:59 +0100

    Finally the right formula.

--- startup.zsh
+++ startup.zsh
@@ -1,6 +1,6 @@
 # Tmux
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  exec tmux attach || exec tmux
+  exec $((tmux attach)&>/dev/null || tmux)
 fi
 
 # NVM
