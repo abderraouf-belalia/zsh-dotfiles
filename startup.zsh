c82ddefe15438d46298e342d0e8ccdba63e7e404
Abderraouf Belalia <abderraoufbelalia@gmail.com>
2024-03-11 15:56:48 +0100

    Reverted.

--- startup.zsh
+++ startup.zsh
@@ -1,6 +1,6 @@
 # Tmux
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  tmux attach || tmux
+  exec tmux attach || exec tmux
 fi
 
 # NVM
